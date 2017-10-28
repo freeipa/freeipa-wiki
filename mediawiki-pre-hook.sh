43054c7e2d3c5930e882c082c013eee1e3049d5e
Martin Kosek <mkosek@redhat.com>
2017-10-28 09:14:20 +0200

    Add symlinks also for old doc archives

--- mediawiki-pre-hook.sh
+++ mediawiki-pre-hook.sh
@@ -4,4 +4,6 @@ set -e
 echo "Mediawiki pre-hook"
 mkdir -p /opt/app-root/data/images
 mkdir -p /opt/app-root/data/downloads
+mkdir -p /opt/app-root/data/docs/1.2/archive
+mkdir -p /opt/app-root/data/docs/2.0.0/archive
 echo "Mediawiki pre-hook OK"
