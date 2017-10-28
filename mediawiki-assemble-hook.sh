faf2d7b74f7810b0ada6dc522d19951b77ced3df
Martin Kosek <mkosek@redhat.com>
2017-10-28 09:36:25 +0200

    Fix docs archive path in assemble script

--- mediawiki-assemble-hook.sh
+++ mediawiki-assemble-hook.sh
@@ -9,7 +9,7 @@ cp ${HOME}/mediawiki-httpd.conf /opt/app-root/etc/conf.d/
 # create symlinks for permanent directories
 ln -s /opt/app-root/data/images/ /opt/app-root/src/php/images
 ln -s /opt/app-root/data/downloads/ /opt/app-root/src/php/downloads
-ln -s /opt/app-root/data/docs/1.2/archive /opt/app-root/src/docs/1.2/archive
-ln -s /opt/app-root/data/docs/2.0.0/archive /opt/app-root/src/docs/2.0.0/archive
+ln -s /opt/app-root/data/docs/1.2/archive /opt/app-root/src/php/docs/1.2/archive
+ln -s /opt/app-root/data/docs/2.0.0/archive /opt/app-root/src/php/docs/2.0.0/archive
 
 echo "Mediawiki assemble hook OK"
