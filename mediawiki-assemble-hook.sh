fc9c71e58954c89cb660c7480cceb5ef3a910b41
Martin Kosek <mkosek@redhat.com>
2017-10-28 09:01:01 +0200

    Assemble hook should not override target

--- mediawiki-assemble-hook.sh
+++ mediawiki-assemble-hook.sh
@@ -7,7 +7,7 @@ echo "Mediawiki assemble hook"
 cp ${HOME}/mediawiki-httpd.conf /opt/app-root/etc/conf.d/
 
 # create symlinks for permanent directories
-ln -sf /opt/app-root/data/images/ /opt/app-root/src/php/images
-ln -sf /opt/app-root/data/downloads/ /opt/app-root/src/php/downloads
+ln -s /opt/app-root/data/images/ /opt/app-root/src/php/images
+ln -s /opt/app-root/data/downloads/ /opt/app-root/src/php/downloads
 
 echo "Mediawiki assemble hook OK"
