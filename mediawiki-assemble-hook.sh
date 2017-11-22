31fc1cdfb18b4bc6af96c642a1051c2a71761c26
Martin Kosek <mkosek@redhat.com>
2017-11-22 14:24:32 +0100

    Stop including vendor libraries
    Download vendor libraries during container build, to make sure that it uses the right
    versions for given PHP.
    
    Include selected PEAR libraries needed for mail support

--- mediawiki-assemble-hook.sh
+++ mediawiki-assemble-hook.sh
@@ -12,4 +12,9 @@ ln -s /opt/app-root/data/downloads/ /opt/app-root/src/php/downloads
 ln -s /opt/app-root/data/docs/1.2/archive /opt/app-root/src/php/docs/1.2/archive
 ln -s /opt/app-root/data/docs/2.0.0/archive /opt/app-root/src/php/docs/2.0.0/archive
 
+# Install wiki dependencies
+./composer.phar require -d /opt/app-root/src/php/ --no-update pear/mail pear/net_smtp
+./composer.phar install -d /opt/app-root/src/php/ --no-ansi --optimize-autoloader
+fix-permissions ./
+
 echo "Mediawiki assemble hook OK"
