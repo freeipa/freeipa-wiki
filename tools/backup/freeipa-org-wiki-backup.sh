512c2e2aaa1441fe822d71aa36e02e0adfb27513
Martin Kosek <mkosek@redhat.com>
2018-01-03 10:09:13 +0100

    Add tools directory with backup function
    The backup script and backup lambda function is the new tool for
    FreeIPA Wiki infrastructure that can take regular backups and
    upload them to S3.
    
    Ticket: freeipa-wiki:#7

---
+++ tools/backup/freeipa-org-wiki-backup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+if [ ! -r ~/.freeipa_org_wiki_config ]; then
+    echo "Cannot read ~/.freeipa_org_wiki_config" >&2
+    exit 1
+else
+    source ~/.freeipa_org_wiki_config
+fi
+
+WORK_DIR=`mktemp -d`
+
+function finish {
+    rm -rf "$WORK_DIR"
+}
+trap finish EXIT
+
+BACKUP_ARCHIVE_NAME="freeipa_org_wiki_`date +%Y%m%d-%H%M%S`.tar.gz"
+BACKUP_ARCHIVE_PATH="$WORK_DIR/$BACKUP_ARCHIVE_NAME"
+
+export KUBECONFIG=$WORK_DIR/kubeconfig
+oc login $OPENSHIFT_SERVER --token="$OPENSHIFT_TOKEN" > /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Cannot login to OpenShift" >&2
+    exit 1
+fi
+
+oc project $OPENSHIFT_PROJECT > /dev/null
+
+db_pod=`oc get pods -o name | grep mysql | cut -d"/" -f 2`
+
+oc port-forward $db_pod 3306 > /dev/null &
+port_forward_ret=$?
+port_forward_pid=$!
+
+if [ $port_forward_ret -ne 0 ]; then
+    echo "Could not port-forward" >&2
+    exit 1
+fi
+
+sleep 5
+mysqldump -h127.0.0.1 -p3306 -u $OPENSHIFT_DATABASE_USERNAME  -p$OPENSHIFT_DATABASE_PASSWORD --single-transaction --quick --routines --triggers $OPENSHIFT_DATABASE_NAME > $WORK_DIR/$OPENSHIFT_DATABASE_NAME.sql
+
+mysqldump_ret=$?
+kill $port_forward_pid > /dev/null
+
+if [ $mysqldump_ret -ne 0 ]; then
+    echo "mysqldump failed" >&2
+    exit 1
+fi
+
+web_pod=`oc get pods -o name | grep freeipa-org-wiki | grep -v build |  cut -d"/" -f 2`
+
+oc rsync -q $web_pod:/opt/app-root/data/images $WORK_DIR/
+
+if [ $? -ne 0 ]; then
+    echo "oc rsync failed" >&2
+    exit 1
+fi
+
+pushd $WORK_DIR > /dev/null
+tar -czf $BACKUP_ARCHIVE_PATH $OPENSHIFT_DATABASE_NAME.sql images/ > /dev/null
+popd
+
+aws s3 cp $BACKUP_ARCHIVE_PATH s3://$S3_BUCKET/$S3_BUCKET_BACKUP_PATH/
+
+if [ $? -ne 0 ]; then
+    echo "aws s3 cp failed" >&2
+    exit 1
+fi
---
+++ tools/backup/lambda_function.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+import boto3
+import logging
+import time
+
+class BackupError(Exception):
+    pass
+
+ec2_configuration = {
+    'ec2_region': 'eu-central-1',
+    'controller_instance_name': 'freeipa-org-wiki-controller',
+    'controller_backup_script_path': '/root/bin/freeipa-org-wiki-backup.sh',
+    'controller_backup_script_timeout': 120,    # seconds
+}
+
+def wiki_backup(debug):
+    logger = logging.getLogger('www.freeipa.org')
+    if debug:
+        logger.setLevel(logging.DEBUG)
+        level = logging.DEBUG
+    else:
+        level = logging.WARNING
+
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+    formatter = logging.Formatter('%(levelname)s: %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    searched_name = ec2_configuration['controller_instance_name']
+    logger.debug("Searching for instance with name '{}'".format(searched_name))
+    ec2 = boto3.resource('ec2', region_name=ec2_configuration['ec2_region'])
+    instances = ec2.instances.filter(Filters=[{
+        'Name': 'tag:Name',
+        'Values': [searched_name]}])
+    instances = list(instances.all())
+
+    if len(instances) != 1:
+        raise BackupError("There must be just one instance named '{}'. Found {}.".format(
+            searched_name, len(instances)))
+
+    controller_instance = instances[0]
+    logger.debug("Found controller instance {}".format(controller_instance.id))
+
+    logger.debug("Executing backup script")
+    ssm_client = boto3.client('ssm')
+    commands = [ec2_configuration['controller_backup_script_path']]
+
+    resp = ssm_client.send_command(
+        DocumentName="AWS-RunShellScript",
+        Parameters={'commands': commands},
+        InstanceIds=[controller_instance.id],
+        TimeoutSeconds=ec2_configuration['controller_backup_script_timeout'],
+        Comment="Backup up FreeIPA Wiki"
+        )
+    command_id = resp['Command']['CommandId']
+
+    logger.debug("Waiting until command {} finishes".format(command_id))
+    command_result = None
+    while True:
+        r = ssm_client.list_commands(CommandId=command_id)
+        command_result = r['Commands'][0]
+        if command_result['CompletedCount'] > 0:
+            status = command_result['Status']
+            logger.debug("Done with status {}, finish waiting".format(status))
+            break
+        time.sleep(5)
+
+    logger.debug("Get invocation details")
+    command_details = ssm_client.get_command_invocation(CommandId=command_id,
+                                                        InstanceId=controller_instance.id)
+    logger.debug("Command run from {} to {}, with status details {}".format(
+        command_details['ExecutionStartDateTime'],
+        command_details['ExecutionEndDateTime'],
+        command_details['StatusDetails']))
+    logger.debug("Command Standard output: {}".format(command_details['StandardOutputContent']))
+    if command_details['StandardErrorContent']:
+        logger.warning("Command Error output: {}".format(command_details['StandardErrorContent']))
+
+    if command_result['Status'] != 'Success':
+        raise BackupError("Backup Command failed: {}".format(command_details['StandardErrorContent']))
+
+    logger.info("Backup finished with status {}".format(command_result['Status']))
+
+if __name__ == '__main__':
+    wiki_backup(debug=True)
+
+# Triggered by Amazon AWS
+def handler(event, context):
+    print("Run FreeIPA Wiki Backup Lambda", event, context)
+    wiki_backup(debug=False)
+    print("FreeIPA Wiki Backup Lambda finished")
+    return {'message': "FreeIPA Wiki Backup sucessful"}
