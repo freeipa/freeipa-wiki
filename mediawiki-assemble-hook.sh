4c1549c80ad1eea941426e306d6d4435f72c5b03
Martin Kosek <mkosek@redhat.com>
2020-03-09 13:40:41 +0100

    composer local install - do not depend on exact signature
    The script adapted from
    https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md

--- mediawiki-assemble-hook.sh
+++ mediawiki-assemble-hook.sh
@@ -59,8 +59,17 @@ fi
 
 if [ $local_run -eq 1 ]; then
     echo "Install composer.phar"
+    EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    php -r "if (hash_file('sha384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+    if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+    then
+        >&2 echo 'ERROR: Invalid installer checksum'
+        rm composer-setup.php
+        exit 1
+    fi
+
     php composer-setup.php
     php -r "unlink('composer-setup.php');"
 fi
